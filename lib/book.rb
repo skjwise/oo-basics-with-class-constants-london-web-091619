27861586122aaa757d0b700d2840eb95ce49719c
Sarah Wise <skjwise@hotmail.com>
2019-09-17 21:21:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,15 +6,15 @@ class Book
     @title = title
   end
   
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
   GENRES = [ ]
   
   def genre(genre)
     @genre = genre
     GENRES << genre
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
   end
 
 end
